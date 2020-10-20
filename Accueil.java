dba53447d29451f03b808564a5c6346c62a5fbde
GAUTHIERNelson <nelson_gauthier@yahoo.com>
2020-10-20 19:36:31 +0200

    Première classe
    Signed-off-by: GAUTHIERNelson <nelson_gauthier@yahoo.com>

---
+++ Accueil.java
@@ -0,0 +1,29 @@
+
+public interface Accueil {
+	
+	public void ajouterClient ();
+	
+	public void modifierClient ();
+	
+	public void supprimerClient ();
+	
+	public void ajouterReserveChambre ();
+	
+	public void modifierReserveChambre ();
+	
+	public void supprimerReserveChambre ();
+	
+	public void ajouterFacture ();
+	
+	public void modifierFacture ();
+	
+	public void supprimerFacture ();
+	
+	public void chercherClient ();
+	
+	public void chercherReserve ();
+	
+	public void chercherChambre ();
+	
+	public void chercherFacture ();
+}
---
+++ Directeur.java
@@ -0,0 +1,33 @@
+
+public class Directeur extends Utilisateur implements Accueil {
+	private int idUtilisateur;
+	private String role;
+	
+	
+	public int getIdUtilisateur() {
+		return idUtilisateur;
+	}
+
+
+	public void setIdUtilisateur(int idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
